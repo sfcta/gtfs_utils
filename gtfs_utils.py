f17c612358e794e9380db0c532ec07c47551fbdd
Drew <drew.cooper@sfcta.org>
2016-04-07 17:42:57 -0700

    1. move unfinished functions to the end 2. get stop_sequence_cols and used_stops only after filtering to weekday only 3. include pattern_id in get_route_statistics groupings 4. filter out *some* outliers in route statistics
    Signed-off-by: Drew <drew.cooper@sfcta.org>

--- gtfs_utils.py
+++ gtfs_utils.py
@@ -84,12 +84,13 @@ class GTFSFeed(object):
                 print "%s not found in %s" % (file, self.path)
                 
         # Useful GTFS manipulations
-        self.stop_sequence_cols = self._get_stop_sequence_cols()
         self.weekday_service_ids= self._get_weekday_service_ids()
-        self.used_stops         = self._get_used_stops()
         if self.weekday_only:
             self.trips = self.trips[self.trips['service_id'].isin(self.weekday_service_ids)]
             self.stop_times = self.stop_times[self.stop_times['trip_id'].isin(self.trips['trip_id'].tolist())]
+        
+        self.stop_sequence_cols = self._get_stop_sequence_cols()
+        self.used_stops         = self._get_used_stops()
 
     def write(self, path='.', ext=None):
         for name, file in itertools.izip(self.all_names, self.all_files):
@@ -161,27 +162,6 @@ class GTFSFeed(object):
 
         self.all_files += ['route_trips.txt', 'stop_routes.txt', 'route_patterns.txt', 'trip_patterns.txt', 'stop_patterns.txt', 'route_statistics.txt', 'stop_statistics.txt']
         self.all_names += ['route_trips', 'stop_routes', 'route_patterns', 'trip_patterns', 'stop_patterns', 'route_statistics', 'stop_statistics']
-##    def drop_deadheads(self, freq_threshold=0.25, similarity_threshold=0.5, how='and'):
-##        if how == 'and':
-##            pass
-##
-##        elif how == 'or':
-##            pass
-##        
-##    def _assign_direction(self):
-##        '''
-##        if direction is missing, assign inbound/outbound
-##        '''
-##        patterns = self._get_route_patterns()
-##        #patterns = self._get_similarity_index(patterns)
-##        first_pattern = patterns.groupby(['route_id']).first().reset_index()
-##        patterns = patterns.set_index(['route_id']+self.stop_sequence_cols)
-##        patterns['direction_id'] = -1
-##        first_pattern = first_pattern.set_index(['route_id']+self.stop_sequence_cols)
-##        patterns.loc[first_pattern.index,'direction_id'] = 0 #patterns['direction_id']
-##        patterns = self._get_similarity_index(patterns.reset_index())
-##        patterns.loc[first_pattern['similarity_index'] >= 1,['direction_id']] = 0
-##
 
     def spatial_match_stops(self, left, right):
         pass
@@ -189,19 +169,6 @@ class GTFSFeed(object):
     def _drop_stops_no_times(self):
         self.stop_times = self.stop_times[(pd.isnull(self.stop_times['arrival_time']) != True)
                                           & (pd.isnull(self.stop_times['departure_time']) != True)]
-
-##    def is_aligned_stop_sequence(self):
-##        '''
-##        return true if there is a set correspondence between stop_id and stop_sequence for all routes
-##        '''
-##        pass
-##
-##    def align_stop_sequence(self):
-##        '''
-##        reassign stop_sequences so there is a unique correspondence between stop_id and stop_sequence for all routes
-##        '''
-##        rts = pd.merge(self.routes, self.trips, on=['route_id'])
-##
 
     def drop_weekend(self):
         self.weekday_only = True
@@ -249,9 +216,6 @@ class GTFSFeed(object):
     
     def apply_time_periods(self, time_periods):
         # update column collections
-        #self._route_trip_idx_cols += ['trip_departure_tp']
-        #self._route_dir_idx_cols += ['trip_departure_tp']
-        #self._trip_idx_cols += ['trip_departure_tp']
         self._tp_idx_cols += ['trip_departure_tp']
         self.time_periods = time_periods
         self.has_time_periods = True        
@@ -288,8 +252,9 @@ class GTFSFeed(object):
         self.trips = self.trips.reset_index()
                 
     def _get_route_statistics(self):
-        grouped = self.route_trips.fillna(-1).groupby(self._route_dir_idx_cols+self._tp_idx_cols)
-
+        grouped = self.route_trips.fillna(-1).groupby(self._route_dir_idx_cols+['pattern_id']+self._tp_idx_cols)
+        rte_dir_pattern_tp_cols = self._route_dir_idx_cols+['pattern_id']+self._tp_idx_cols
+        rte_dir_pattern_cols    = self._route_dir_idx_cols+['pattern_id']
         # calculate average headways / frequencies based on number of runs and length of time period
         route_statistics = grouped.sum()
         route_statistics = pd.DataFrame(route_statistics,columns=[])
@@ -301,13 +266,13 @@ class GTFSFeed(object):
                 start, stop = HHMMSSpair_to_MPMpair(hhmmsspair)
                 length = round(stop-start,0)
                 route_statistics.loc[route_statistics['trip_departure_tp'] == tp,'period_len_minutes'] = length
-            route_statistics = route_statistics.set_index(self._route_dir_idx_cols+self._tp_idx_cols)
+            route_statistics = route_statistics.set_index(self._route_dir_idx_cols+['pattern_id']+self._tp_idx_cols)
             route_statistics['freq'] = 60 * route_statistics['trips'] / route_statistics['period_len_minutes']
             route_statistics['avg_headway'] = route_statistics['period_len_minutes'] / route_statistics['trips']
             route_statistics = route_statistics.reset_index()
-            pivot = route_statistics.pivot_table(index=self._route_dir_idx_cols,columns=self._tp_idx_cols,values=['trips','freq','avg_headway'])
+            pivot = route_statistics.pivot_table(index=self._route_dir_idx_cols+['pattern_id'],columns=self._tp_idx_cols,values=['trips','freq','avg_headway'])
             route_statistics = pd.DataFrame(self.route_patterns,columns=self._route_dir_idx_cols+['pattern_id']+self._route_pattern_info_cols)
-            route_statistics = route_statistics.set_index(self._route_dir_idx_cols)
+            route_statistics = route_statistics.set_index(self._route_dir_idx_cols+['pattern_id'])
             for stat in ['trips','freq','avg_headway']:
                 for tp in self.time_periods.iterkeys():
                     route_statistics['%s_%s' % (tp, stat)] = pivot[stat][tp]
@@ -321,17 +286,39 @@ class GTFSFeed(object):
         sorted_trips['next_pattern'] = sorted_trips['pattern_id'].shift(-1)
         sorted_trips = sorted_trips[sorted_trips['pattern_id'] == sorted_trips['next_pattern']]
         sorted_trips['headway'] = sorted_trips['next_departure'] - sorted_trips['trip_departure_mpm']
+        sorted_trips['outlier'] = 0
         
         if sorted_trips['headway'].lt(0).sum() > 0:
             print "sorted trips have negative headways"
-            #print sorted_trips
             print sorted_trips[sorted_trips['headway'].lt(0)]
             sys.exit()
-        calc_headways = sorted_trips.groupby(self._route_dir_idx_cols+self._tp_idx_cols)
+
+        # find outliers (ex. large gaps in service for routes that only run in peak)
+        grouped = sorted_trips.groupby(self._route_dir_idx_cols+['pattern_id']+self._tp_idx_cols)
+        minmax = []
+        for name, group in grouped:
+            # drop the min and max headways
+            idxmin = group['headway'].idxmin()
+            idxmax = group['headway'].idxmax()
+            minmax.append(idxmin)
+            minmax.append(idxmax)
+
+        no_min_max = sorted_trips[~sorted_trips.index.isin(minmax)]
+        test = no_min_max.groupby(self._route_dir_idx_cols+['pattern_id']+self._tp_idx_cols)['headway'].agg([np.mean,np.std])
+
+        for name, group in grouped:
+            if name not in test.index: continue
+            outliers = group[group['headway'] > (test.loc[name,'mean'] * 2)]
+            if len(outliers > 0):
+                sorted_trips.loc[outliers.index,'outlier'] = 1
+
+        sorted_trips.to_csv('sorted_trips.csv')
+        sorted_trips = sorted_trips[sorted_trips['outlier'] == 0]
+        calc_headways = sorted_trips.groupby(self._route_dir_idx_cols+['pattern_id']+self._tp_idx_cols)
         calc_headways = calc_headways['headway'].agg([np.mean, np.std, np.median, np.min, np.max])
         
         if self.has_time_periods:
-            pivot = calc_headways.reset_index().pivot_table(index=self._route_dir_idx_cols,columns=self._tp_idx_cols,values=['mean','std','median','amin','amax'])
+            pivot = calc_headways.reset_index().pivot_table(index=self._route_dir_idx_cols+['pattern_id'],columns=self._tp_idx_cols,values=['mean','std','median','amin','amax'])
             for stat in ['mean','std','median','amin','amax']:
                 for tp in self.time_periods.iterkeys():
                     route_statistics['%s_%s_headway' % (tp, stat)] = pivot[stat][tp]
@@ -382,24 +369,6 @@ class GTFSFeed(object):
         route_pattern = route_pattern.replace(-1, np.nan)
 
         return route_pattern
-
-##    def _get_representative_trips(self, trips, patterns):
-##        patterns = stop_times.pivot(index='trip_id',columns='stop_sequence',values='stop_id').reset_index()
-##        patterns = patterns.fillna(-1)
-##        columns = patterns.columns.tolist()
-##        grouped_pattern = patterns.groupby(columns)
-##        trip_to_repr_trip = pd.DataFrame(index=self.trips['trip_id'],columns=['repr_trip_id'])
-##        repr_trip_ids = []
-##        for name, group in grouped_pattern:
-##            #print group
-##            repr_trip_id = group['trip_id'].irow(0)
-##            repr_trip_ids.append(repr_trip_id)
-##            all_trip_ids = group['trip_id'].values.tolist()
-##            if len(all_trip_ids) == 1: all_trip_ids = all_trip_ids[0]
-##            trip_to_repr_trip.loc[all_trip_ids,'repr_trip_id'] = repr_trip_id
-##            
-##        self.trip_to_repr_trip = trip_to_repr_trip.reset_index()
-##        self.repr_trips = self.trips[self.trips['trip_id'].isin(repr_trip_ids)]
         
     def _get_stop_sequence_cols(self):
         stop_sequence_cols = list(set(self.stop_times['stop_sequence'].tolist()))
@@ -421,10 +390,8 @@ class GTFSFeed(object):
             if len(maxrow) > 1:
                 print "maxrow contains multiple records, selecting first"
                 maxrow = maxrow[0]
-                
             route_patterns.loc[this_group['count'].idxmax(),'is_base_pattern'] = 1
             route_patterns.loc[this_group.index,'total_base_stops'] = maxrow.loc[:,self.stop_sequence_cols].T.count().sum()
-            #route_patterns.loc[this_group.index,'similar_base_stops'] = this_group.eq(maxrow.values.tolist()[0]).T.sum() - 1
             route_patterns.loc[this_group.index,'similar_base_stops'] = this_group.loc[:,self.stop_sequence_cols].eq(maxrow.loc[:,self.stop_sequence_cols].values.tolist()[0]).T.sum()
         route_patterns['similarity_index'] = route_patterns['similar_base_stops'] / route_patterns['total_base_stops']
         
@@ -443,7 +410,28 @@ class GTFSFeed(object):
                 else:
                     ret = ret + ', %s' % (key)
         return ret
-        
+
+
+##    def is_aligned_stop_sequence(self):
+##        '''
+##        return true if there is a set correspondence between stop_id and stop_sequence for all routes
+##        '''
+##        pass
+##
+##    def align_stop_sequence(self):
+##        '''
+##        reassign stop_sequences so there is a unique correspondence between stop_id and stop_sequence for all routes
+##        '''
+##        rts = pd.merge(self.routes, self.trips, on=['route_id'])
+##
+##    def drop_deadheads(self, freq_threshold=0.25, similarity_threshold=0.5, how='and'):
+##        if how == 'and':
+##            pass
+##
+##        elif how == 'or':
+##            pass
+##        
+
 if __name__=='__main__':
     gtfs = GTFSFeed('..\SFMTA_20120319')
     print gtfs
